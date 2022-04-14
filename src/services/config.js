1a28b86f0a81246246807a07986e95a394621a9a
xiaoyao <1065645460@qq.com>
2022-04-14 20:05:04 +0800

    change

--- src/services/config.js
+++ src/services/config.js
@@ -1,6 +1,6 @@
 // const devBaseURL = "https://netease-cloud-music-api-mrshimmer.vercel.app/";
 // flask内置web服务器
-const devBaseURL = "http://localhost:8001";
+const devBaseURL = "http://localhost:5000";
 // const proBaseURL = "https://netease-cloud-music-api-mrshimmer.vercel.app/";
 const proBaseURL = "http://localhost:8001";
 export const BASE_URL = process.env.NODE_ENV === "development" ? devBaseURL : proBaseURL;
